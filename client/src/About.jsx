c21826640ef5056d486eb7a25cccf5059a2cd032
Christine <christinekhekama99@gmail.com>
2023-12-14 21:03:48 +0300

    adding the about and styling it

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -1,5 +1,6 @@
-// AboutMe.js
+
 import React from 'react';
+
 
 const AboutMe = () => {
   return (
@@ -7,8 +8,12 @@ const AboutMe = () => {
       <h3>About Me</h3>
       <p>
         I specialize in building modern web applications using the latest technologies.
-        With a strong foundation in JavaScript and front-end frameworks like React, I create
+        With a strong foundation in JavaScript and front-end frameworks like React, database management and backend solutions using Python, PostgreSQL and backend-frameworks like Flask. I create
         efficient and scalable solutions.
+      </p>
+      <p className="additional-info">
+        Apart from coding, I am passionate about user interfaces, responsive design, and creating
+        engaging user experiences. Let's turn ideas into reality through clean and elegant code!
       </p>
     </div>
   );
